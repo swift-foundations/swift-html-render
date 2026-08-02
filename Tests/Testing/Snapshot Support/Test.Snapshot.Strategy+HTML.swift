a5bf2b800b0d43449b9d5f66185643aff7adbaa6
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-02 06:01:42 +0200

    Disambiguate HTML snapshot strategy references

--- Tests/Testing/Snapshot Support/Test.Snapshot.Strategy+HTML.swift
+++ Tests/Testing/Snapshot Support/Test.Snapshot.Strategy+HTML.swift
@@ -7,7 +7,7 @@ public import HTML_Rendering_Core
 public import Test_Snapshot_Primitives
 public import WHATWG_HTML_Shared
 
-extension Test.Snapshot.Strategy where Value: HTML.Document.`Protocol`, Format == String {
+extension Test_Primitives_Core.Test.Snapshot.Strategy where Value: HTML.Document.`Protocol`, Format == String {
     public static var html: Self {
         .html()
     }
@@ -15,7 +15,7 @@ extension Test.Snapshot.Strategy where Value: HTML.Document.`Protocol`, Format =
     public static func html(
         configuration: HTML.Context.Configuration = .pretty
     ) -> Self {
-        Test.Snapshot.Strategy<String, String>.lines.pullback { value in
+        Test_Primitives_Core.Test.Snapshot.Strategy<String, String>.lines.pullback { value in
             HTML.Context.Configuration.$current.withValue(configuration) {
                 (try? String(value)) ?? "HTML rendering failed"
             }
@@ -23,7 +23,7 @@ extension Test.Snapshot.Strategy where Value: HTML.Document.`Protocol`, Format =
     }
 }
 
-extension Test.Snapshot.Strategy where Value: HTML.View, Format == String {
+extension Test_Primitives_Core.Test.Snapshot.Strategy where Value: HTML.View, Format == String {
     public static var html: Self {
         .html()
     }
@@ -31,7 +31,7 @@ extension Test.Snapshot.Strategy where Value: HTML.View, Format == String {
     public static func html(
         configuration: HTML.Context.Configuration = .pretty
     ) -> Self {
-        Test.Snapshot.Strategy<String, String>.lines.pullback { value in
+        Test_Primitives_Core.Test.Snapshot.Strategy<String, String>.lines.pullback { value in
             HTML.Context.Configuration.$current.withValue(configuration) {
                 (try? String(value)) ?? "HTML rendering failed"
             }
